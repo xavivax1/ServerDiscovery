fe7fd532d2c79a59e691996fe144ae592df54de3
you name <allianz email>
2022-02-05 12:13:09 +0100

    First Commint including al features

---
+++ src/main/java/com/formacionspring/serverdiscovery/ServerDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.formacionspring.serverdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerDiscoveryApplication.class, args);
+	}
+
+}
